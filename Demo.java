871d9a628e21447e0c9f9df637754015059d73d6
Rohan-147 <rohanjhala.147@gmail.com>
2025-01-02 15:33:20 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Demo {
+public static void main(String[] args) {
+    System.out.println("Hello Rohan");
+    Scanner s= new Scanner(System.in);
+
+    int a= s.nextInt();
+    int b= s.nextInt();
+    int c= s.nextInt();
+
+    if(a > b && a > c) {
+        System.out.println("a is maximum");
+    } else if(b > a && b > c) {
+        System.out.println("b is maximum");
+    } else {
+        System.out.println ("c is maximum");
+    }
+
+}
+    
+}
