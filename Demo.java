d00e6cd55b95d7a1468f79b991338557269e1dc3
Rohan-147 <rohanjhala.147@gmail.com>
2025-01-02 15:34:16 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -12,7 +12,7 @@ public static void main(String[] args) {
 
     if(a > b && a > c) {
         System.out.println("a is maximum");
-    } else if(b > a && b > c) {
+    } else if(b > c) {
         System.out.println("b is maximum");
     } else {
         System.out.println ("c is maximum");
